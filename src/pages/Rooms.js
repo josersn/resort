bc91e86a4bfb58ca5efb118104ab42a43735edd3
José Ramos <nettorammos@hotmail.com>
2020-04-28 15:20:37 -0300

    edit Link  Component

--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -3,13 +3,16 @@ import { Link } from "react-router-dom";
 
 import Banner from "../components/Banner";
 import Hero from "../components/Hero";
-
+import Container from "../components/RoomsContainer";
 const Rooms = () => (
+  <>
   <Hero hero="roomsHero">
     <Banner title="Nossos Quartos">
-      <Link  class="btn-primary" to="/">Voltar para o início</Link>
+      <Link  className="btn-primary" to="/">Voltar para o início</Link>
     </Banner>
   </Hero>
+  <Container />
+  </>
 )
 
 export default Rooms;
